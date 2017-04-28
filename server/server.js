8d93c61cf7d9f216622e98960984fcdb322aa749
Steve <stevenjchang@gmail.com>
2017-04-28 14:26:21 -0700

    Implement app.use serving up static index.html file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.use('/', express.static(__dirname + '/../public'), function(req, res, next) {
+    console.log(req.url);
+    next();
+});
+
 app.route('/')
   .get(function(req, res) {
     res.status(200).send('app.get successful')
